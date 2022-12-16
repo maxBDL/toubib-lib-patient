27ee036a6a48096c02406ea406b289cb56def057
Maxime DIDIER <maxime_didier@aol.com>
2022-12-16 09:36:30 +0100

    add web manifest

--- src/views/Appointment.vue
+++ src/views/Appointment.vue
@@ -12,19 +12,18 @@ defineProps({
 const route = useRouter();
 
 const count = ref(0)
-const lastname = ref(null);
-const name = ref(null);
+// const lastname = ref(null);
+// const name = ref(null);
 const doctor_id = ref(null);
 const description = ref(null);
-const user_id = ref("1");
 const doctors = ref(null);
 const date = ref(null);
+const hasError = ref(false);
 
 onMounted(async () => {
   const result = await getDocs(collection(db, "doctors"));
   const docs = [];
   result.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
 
     const doctor = {
       'id': doc.id,
@@ -41,10 +40,10 @@ onMounted(async () => {
 const createAppointment = async () => {
   try {
     const result = await addDoc(collection(db, "appointments"), {
-      name: name.value,
-      lastname: lastname.value,
+      // name: name.value,
+      // lastname: lastname.value,
       doctor_id: doctor_id.value,
-      user_id: user_id.value,
+      user_id: auth.currentUser.uid,
       description: description.value,
       date: date.value
     });
@@ -63,7 +62,6 @@ const createAppointment = async () => {
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
           <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -81,44 +79,9 @@ const createAppointment = async () => {
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <RouterLink to="/"> Home </RouterLink>
-              <RouterLink to="/appointments"> Nouveau </RouterLink>
+              <RouterLink class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/"> Home </RouterLink>
+              <RouterLink class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" to="/appointments"> Nouveau </RouterLink>
             </div>
-          </div>
-        </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-            <span class="sr-only">View notifications</span>
-            <!-- Heroicon name: outline/bell -->
-            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-            </svg>
-          </button>
-
-          <!-- Profile dropdown -->
-          <div class="relative ml-3">
-            <div>
-              <button type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-              </button>
-            </div>
-
-            <!--
-              Dropdown menu, show/hide based on menu state.
-
-              Entering: "transition ease-out duration-100"
-                From: "transform opacity-0 scale-95"
-                To: "transform opacity-100 scale-100"
-              Leaving: "transition ease-in duration-75"
-                From: "transform opacity-100 scale-100"
-                To: "transform opacity-0 scale-95"
-            -->
-            <!-- <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-
-              <RouterLink to="/"> Home </RouterLink> 
-              <RouterLink to="/appointments"> Nouveau </RouterLink>
-            </div> -->
           </div>
         </div>
       </div>
@@ -135,8 +98,7 @@ const createAppointment = async () => {
   </nav>
 
 
-
-  <div class="mt-10 sm:mt-0">
+  <div >
     <div class="md:grid md:grid-cols-3 md:gap-6">
       <div class="mt-5 md:col-span-2 md:mt-0">
         <form @submit.prevent="createAppointment">
@@ -144,7 +106,7 @@ const createAppointment = async () => {
             <div class="bg-white px-4 py-5 sm:p-6">
               <div class="grid grid-cols-6 gap-6">
 
-                <div class="col-span-6 sm:col-span-3">
+                <!-- <div class="col-span-6 sm:col-span-3">
                   <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
                   <input required v-model="name" type="text" name="name" id="first-name" autocomplete="given-name"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
@@ -155,7 +117,7 @@ const createAppointment = async () => {
                   <input required v-model="lastname" type="text" name="lastname" id="last-name"
                     autocomplete="family-name"
                     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-                </div>
+                </div> -->
 
                 <div class="col-span-6 sm:col-span-3">
                   <label for="country" class="block text-sm font-medium text-gray-700">Doctor</label>
